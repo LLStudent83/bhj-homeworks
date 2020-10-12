7618bbe544aaafe1e22e9cc25a2cb0763aab70a7
Dmitry Prigulnov <prigulnov@mail.ru>
2020-10-12 21:08:11 +0500

    menu var 2

--- element-search/menu/task.js
+++ element-search/menu/task.js
@@ -1,16 +1,13 @@
 const htmlcMenuLink = document.getElementsByClassName("menu__link");
 for (let i = 0; i < htmlcMenuLink.length; i++) {
   htmlcMenuLink[i].onclick = function () {
-    if (
-      document.querySelectorAll(".menu_active") &&
-      !htmlcMenuLink[i].closest("li").querySelector(".menu_active")
+    if (!htmlcMenuLink[i].closest("li").querySelector(".menu_active")
     ) {
-      console.log(document.querySelector(".menu_active"));
       let x = document.querySelector(".menu_active");
       if (x) {
         x.classList.remove("menu_active");
       }
-    }
+        }
     if (htmlcMenuLink[i].closest("li").querySelector("ul.menu_sub")) {
       let classLinkMenu = htmlcMenuLink[i]
         .closest("li")
