52aa33cd057252686869209178511c77ffeab8f6
Dmitry Prigulnov <prigulnov@mail.ru>
2020-10-13 21:23:18 +0500

    1.2 add slider

---
+++ element-search/slider/task.js
@@ -0,0 +1,21 @@
+let arrImg = document.getElementsByClassName("slider__item");
+let button = document.getElementsByClassName("slider__arrow");
+let n = 0;
+
+for(let i = 0;i < button.length;i++) {
+button[i].onclick = function() {
+if(button[i].classList.contains("slider__arrow_prev")) {
+    arrImg[n].classList.remove("slider__item_active");
+    n = n - 1;
+  n < 0 ? n = arrImg.length - 1 : undefined;
+arrImg[n].classList.add("slider__item_active");
+};
+
+if(button[i].classList.contains("slider__arrow_next")) {
+    arrImg[n].classList.remove("slider__item_active");
+    n = n + 1;
+  n > arrImg.length - 1 ? n = 0 : undefined;
+arrImg[n].classList.add("slider__item_active");
+};
+};
+};            
